05bc593666ba222199c38c51189c0cce54718c96
0xymoro <jerrymeng100@gmail.com>
2025-10-15 16:23:35 -0700

    Guard camera capture in app extensions

--- Sources/LiveKit/Participant/LocalParticipant.swift
+++ Sources/LiveKit/Participant/LocalParticipant.swift
@@ -364,20 +364,16 @@ public extension LocalParticipant {
                 }
             } else if enabled {
                 // Try to create a new track
-                #if APP_EXTENSION
                 if source == .camera {
+                    #if APP_EXTENSION
                     self.log("Camera capture is not supported in app extensions.", .warning)
                     return nil
-                }
-                #else
-                if source == .camera {
+                    #else
                     let localTrack = LocalVideoTrack.createCameraTrack(options: (captureOptions as? CameraCaptureOptions) ?? room._state.roomOptions.defaultCameraCaptureOptions,
                                                                        reportStatistics: room._state.roomOptions.reportRemoteTrackStatistics)
                     return try await self._publish(track: localTrack, options: publishOptions)
-                }
-                #endif
-
-                if source == .microphone {
+                    #endif
+                } else if source == .microphone {
                     let localTrack = LocalAudioTrack.createTrack(options: (captureOptions as? AudioCaptureOptions) ?? room._state.roomOptions.defaultAudioCaptureOptions,
                                                                  reportStatistics: room._state.roomOptions.reportRemoteTrackStatistics)
                     return try await self._publish(track: localTrack, options: publishOptions)
