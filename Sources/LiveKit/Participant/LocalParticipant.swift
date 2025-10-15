ec3049bee8eff44ff0d9195f8336e45b0293eb65
0xymoro <jerrymeng100@gmail.com>
2025-10-15 15:57:41 -0700

    fix: short-circuit camera publish in extensions

--- Sources/LiveKit/Participant/LocalParticipant.swift
+++ Sources/LiveKit/Participant/LocalParticipant.swift
@@ -364,15 +364,18 @@ public extension LocalParticipant {
                 }
             } else if enabled {
                 // Try to create a new track
+                #if APP_EXTENSION
                 if source == .camera {
-                    if #available(iOSApplicationExtension 13.0, *) {
-                        self.log("Camera capture is not supported in app extensions.", .warning)
-                        return nil
-                    }
+                    self.log("Camera capture is not supported in app extensions.", .warning)
+                    return nil
+                } else if source == .microphone {
+                #else
+                if source == .camera {
                     let localTrack = LocalVideoTrack.createCameraTrack(options: (captureOptions as? CameraCaptureOptions) ?? room._state.roomOptions.defaultCameraCaptureOptions,
                                                                        reportStatistics: room._state.roomOptions.reportRemoteTrackStatistics)
                     return try await self._publish(track: localTrack, options: publishOptions)
                 } else if source == .microphone {
+                #endif
                     let localTrack = LocalAudioTrack.createTrack(options: (captureOptions as? AudioCaptureOptions) ?? room._state.roomOptions.defaultAudioCaptureOptions,
                                                                  reportStatistics: room._state.roomOptions.reportRemoteTrackStatistics)
                     return try await self._publish(track: localTrack, options: publishOptions)
